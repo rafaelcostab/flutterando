3050762413212361150c3668944b76d9da8cad35
Rafael Costa <rafael.costab@hotmail.com>
2023-06-01 22:40:26 -0300

    10: the scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,18 @@ class HomeWidget extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: const Center(
         child: Text('Flutterando'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          debugPrint('Clicou');
+        },
       ),
     );
   }
