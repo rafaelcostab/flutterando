fd1cda1af33d90711500f31ebeaa3880b8d2475a
Rafael Costa <rafael.costab@hotmail.com>
2023-06-01 22:28:57 -0300

    9: errorlens and lint

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context){
-    return MaterialApp(
+    return const MaterialApp(
       home: HomeWidget(),
     );
   }
 }
 
 class HomeWidget extends StatelessWidget{
+  const HomeWidget({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Material(
+    return const Material(
       child: Center(
         child: Text('Flutterando'),
       ),
